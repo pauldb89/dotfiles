2fe677cb8911e407cd277a8f0d64c6d81c56be9e
Paul Baltescu <pauldb89@gmail.com>
2022-08-08 14:26:39 -0700

    Time pulling docker image.

--- remote/setup_2.sh
+++ remote/setup_2.sh
@@ -2,4 +2,4 @@ echo "Logging into Gitlab..."
 docker login registry.gitlab.com
 
 echo "Pulling docker image..."
-docker pull registry.gitlab.com/mldocker/ml
+time docker pull registry.gitlab.com/mldocker/ml
